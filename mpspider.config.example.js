6e6f68923c0460d69af68a4af90f523ac869d209
wangyongqing01 <wangyongqing01@baidu.com>
2019-02-14 13:52:55 +0800

    增加 prettier

--- mpspider.config.example.js
+++ mpspider.config.example.js
@@ -1,4 +1,30 @@
 module.exports = {
+    turndown: {
+        rule: {
+            code: {
+                filter: ['tr'],
+                replacement: content => {
+                    content = content.trim();
+                    if (/`$/.test(content) && /^1/.test(content)) {
+                        const rs = content.split(/\n/);
+                        const code = rs.splice(rs.length / 2 + 1);
+                        const realCode = [];
+                        for (let i = 0, len = rs.length; i < len; i++) {
+                            if (rs[i].trim() && code[i]) {
+                                realCode.push(code[i].replace(/`/g, ''));
+                            }
+                        }
+                        content = `
+\`\`\`
+${realCode.join('\n')}
+\`\`\`
+`;
+                    }
+                    return content;
+                }
+            }
+        }
+    },
     afterConverter: content => {
         return content.replace(/<(.+?)>/g, (i, m) => {
             return `&lt;${m}>`;
